8979e93743377c2a18ff4b830da774c17ea4d96c
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2025-01-28 21:30:14 +0200

    done with search bar design, need to add calendar functionality

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -2,11 +2,13 @@ import white_logo from './white_logo.svg'
 import globe from './globe.svg'
 import menu from './menu.svg'
 import avatar from './avatar.svg'
+import search_icon from './search_icon.svg'
 
 export const assets = {
     white_logo,
     globe,
     menu,
-    avatar
+    avatar,
+    search_icon
 
 }
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='max-w-[848px] w-full flex gap-[32px] h-[64px] rounded-[1000px] py-[8px] pl-[32px] pr-[8px] bg-white items-center'>
+      {/* first section */}
+      <div className='max-w-[193px] w-full h-[36px]'>
+        <p className='h-[16px] font-600 text-[12px] leading-[16px] text-black pl-1'>Locations</p>
+        <select className='h-[20px] font-400 text-[14px] leading-[20px] text-[#6B7280] outline-none pr-16'>
+        <option value="Sandton">Select a Location</option>
+          <option value="Sandton">Sandton</option>
+          <option value="Johannesburg">Johannesburg</option>
+          <option value="Pretoria">Pretoria</option>
+          <option value="Alberton">Alberton</option>
+        </select>
+      </div>
+      <div className='h-[32px] border-[1px]  bg-[#6B7280] border-[#E5E7EB]' />
+
+      {/* second section */}
+      <div className='max-w-[114.3px] w-full h-[36px]'>
+        <p className='h-[16px] font-600 text-[12px] leading-[16px] text-black pl-1'>Check in</p>
+        <p className='h-[20px] font-400 text-[14px] leading-[20px] text-[#6B7280] outline-none'>Add dates</p>
+      </div>
+      <div className='h-[32px] border-[1px]  bg-[#6B7280] border-[#E5E7EB]' />
+
+      {/* Third section */}
+      <div className='max-w-[114.3px] w-full h-[36px]'>
+        <p className='h-[16px] font-600 text-[12px] leading-[16px] text-black pl-1'>Check in</p>
+        <p className='h-[20px] font-400 text-[14px] leading-[20px] text-[#6B7280] outline-none'>Add dates</p>
+      </div>
+      <div className='h-[32px] border-[1px]  bg-[#6B7280] border-[#E5E7EB]' />
+
+      {/* fourth section */}
+      <div className='max-w-[114.3px] w-full h-[36px]'>
+        <p className='h-[16px] font-600 text-[12px] leading-[16px] text-black pl-1'>Guests</p>
+        <p className='h-[20px] font-400 text-[14px] leading-[20px] text-[#6B7280] outline-none'>Add guests</p>
+      </div>
+      <img className="max-w-[48px] w-ful h-[48px]"src={assets.search_icon} alt="" />
+    </div>
   )
 }
 
 export default Search
+
+
+
+
