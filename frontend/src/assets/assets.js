c132d841fc2a70889dc811c3da770c60483c63b0
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2025-02-14 12:39:01 +0200

    react project setup

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,15 @@
+import airbnb_logo from './Vector.png'
+import globe from './globe.png'
+import hero from './hero.jpg'
+import menu from './menu.png'
+import search from './search.png'
+import down_arrow from './down-arrow.png'
+
+export const assets = {
+    airbnb_logo,
+    globe,
+    hero,
+    menu,
+    search,
+    down_arrow
+}
