8283fd4f32eae5fa1052223b8599afbe724f131f
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2025-02-14 12:45:06 +0200

    Create setup for homepage and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
     </div>
   )
 }
