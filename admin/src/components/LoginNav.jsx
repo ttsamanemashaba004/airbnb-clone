d2fa7b6b3b2bdd9f1335d068198a462eb7b583f2
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2025-03-20 23:29:57 +0200

    Admin addition to frontend

--- admin/src/components/LoginNav.jsx
+++ admin/src/components/LoginNav.jsx
@@ -3,8 +3,9 @@ import './LoginNav.css'
 import { assets } from '../assets/assets'
 
 const LoginNav = () => {
+  const frontUrl = 'https://airbnb-clone-frontend-roan.vercel.app/';
   return (
-    <div className='login-nav-container'>
+    <div className='login-nav-container' onClick={() => window.location.href = frontUrl}>
         <img src={assets.logo} alt="" className='nav-img'/>
     </div>
   )
